1f2254af9304202722bc24b8690dd447a2132d4a
ojotoxy <ojotoxy@gmail.com>
2017-02-21 22:56:12 -0800

    Update run_content_network.lua

--- run_content_network.lua
+++ run_content_network.lua
@@ -30,7 +30,7 @@ local real_ctx = torch.Tensor(32,3,128,128)
 local fake2 = torch.Tensor(3,256,256)
 local output=torch.Tensor(3,512,512)
 
-for i=1,21 do
+for i=1,32 do
     real[i]=loadImage(string.format('examples/input_%04d.png',i),512)
     real_ctx[i]:copy(image.scale(real[i],128,128))
     real_ctx[{i,{1},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred}}] = 2*117/255.0 - 1.0
@@ -38,7 +38,7 @@ for i=1,21 do
     real_ctx[{i,{3},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred}}] = 2*123/255.0 - 1.0
 end
 fake = modelG:forward(real_ctx)
-for i=1,21 do
+for i=1,32 do
     fake2:copy(image.scale(fake[i],256,256))
     output:copy(real[i])
     output[{{},{145,368},{145,368}}]:copy(fake2[{{},{17, 240},{17, 240}}])
