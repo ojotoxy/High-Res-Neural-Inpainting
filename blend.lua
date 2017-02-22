e6ca29a0696e77596f02511be9dfecccfb3247ee
ojotoxy <ojotoxy@gmail.com>
2017-02-21 22:29:21 -0800

    Update blend.lua

--- blend.lua
+++ blend.lua
@@ -1,6 +1,6 @@
 require 'image'
 
-for i=1,32 do
+for i=1,1 do
     src=image.load(string.format('examples/input_%04d.png',i))
     tgt=image.load(string.format('examples/demo_input_%04d.pngTOfake_%04d.png.jpg',i,i))
     mask=image.load('mask.png')
